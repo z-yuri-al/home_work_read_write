346959d7065c439a05f33f6dea70aa191c361f3d
Yuri Zyev <z.yuri.al@gmail.com>
2023-05-11 15:40:55 +0300

    finish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from pprint import pprint
-
+#ЗАДАНИЕ 1
 with open('recipes.txt', 'rt') as file:
     cook_book = {}
     for line in file:
@@ -9,9 +9,9 @@ with open('recipes.txt', 'rt') as file:
         for _ in range(count_ingredient):
             ingredient_name, quantity, measure = file.readline().strip().split(' | ')
             ingredient_dict = {
-                'ingredient_name' : ingredient_name,
-                'quantity' : quantity,
-                'measure' : measure
+                'ingredient_name': ingredient_name,
+                'quantity': quantity,
+                'measure': measure
             }
             ingredient_list.append(ingredient_dict)
         file.readline()
@@ -22,6 +22,7 @@ with open('recipes.txt', 'rt') as file:
 print()
 print()
 
+#ЗАДАНИЕ 2
 def get_shop_list_by_dishes(dishes, person_count):
     result = {}
     for dish in dishes:
@@ -30,8 +31,8 @@ def get_shop_list_by_dishes(dishes, person_count):
             key = ingridient['ingredient_name']
             if key not in result:
                 new_dict = {
-                    'measure' : ingridient['measure'],
-                    'quantity' : int(ingridient['quantity']) * person_count
+                    'measure': ingridient['measure'],
+                    'quantity': int(ingridient['quantity']) * person_count
 
                 }
                 result[key] = new_dict
@@ -45,27 +46,34 @@ get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'
 
 #ЗАДАНИЕ 3
 
-with open('sorted/1.txt') as file1, open('sorted/2.txt') as file2, open('sorted/3.txt') as file3, open('sorted/new.txt', 'w') as new_file:
-    print(file2.read())
+import os
+
+current = os.getcwd()
+folder = 'sorted'
+file_name_1 = '1.txt'
+file_name_2 = '2.txt'
+file_name_3 = '3.txt'
+file_name_4 = 'new.txt'
+
+with open(os.path.join(current, folder, file_name_1)) as file1, open(os.path.join(current, folder, file_name_2)) as file2, open(os.path.join(current, folder, file_name_3)) as file3, open(os.path.join(current, folder, file_name_4), 'w') as new_file:
     dict_file = {
-        file1 : len(file1.readlines()),
-        file2 : len(file2.readlines()),
-        file3 : len(file3.readlines())
+        file1 : [len(file1.readlines()), file_name_1],
+        file2 : [len(file2.readlines()), file_name_2],
+        file3 : [len(file3.readlines()), file_name_3]
     }
 
+    dict_file_sort = dict(sorted(dict_file.items(), key=lambda item : item[1][0]))
+    # pprint(dict_file_sort, sort_dicts=False)
+    file1.seek(0)
+    file2.seek(0)
+    file3.seek(0)
 
-    dict_file_sort = dict(sorted(dict_file.items(), key=lambda item: item[1]))
-    pprint(dict_file_sort, sort_dicts=False)
-    print()
-
-
-    print(file2.read())
-
-    for file, len_file in dict_file_sort.items():
-        new_file.writelines('file_name\n')
-        new_file.writelines(f'{len_file}\n')
-        for line in file.readlines():
-            new_file.writelines(f'{line}\n')
+    for keys, values in dict_file_sort.items():
+        new_file.writelines(f'{values[1]}\n')
+        new_file.writelines(f'{values[0]}\n')
+        for line in keys.readlines():
+            new_file.writelines(f'{line}')
+        new_file.writelines('\n')
 
 
 
